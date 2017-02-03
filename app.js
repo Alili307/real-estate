950b629f2a36c71e8f0d4a798163fd841db49566
Alili307 <aliciamathen@orange.fr>
2017-02-03 15:52:01 +0100

    Commit 1
    Commit 1.1

--- app.js
+++ app.js
@@ -21,7 +21,54 @@ app.get( '/', function ( req, res ) {
 });
 
 
-//launch the server on the 3000 port
-app.listen( 3000, function () {
-    console.log( 'App listening on port 3000!' );
+//launch the server on the 4000 port
+app.listen( 4000, function () {
+    console.log( 'App listening on port 4000!' );
 });
+
+function lbc() {
+
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1087631966.htm?ca=12_s'
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+
+            const $ = cheerio.load( html )
+
+            const lbcDataArray = $( 'section.properties span.value' )
+
+            let lbsData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 ),
+            }
+            console.log( lbcData )
+        }
+        else {
+            console.log( error )
+        }
+    })
+}
+
+
+app.get( '/', function ( req, res ) {
+
+    var url = 'req.query.urlLBC' // html page url
+
+    
+    lbc();
+
+    res.render( 'home', {
+        message: 'The Home Page!'
+    });
+
+
+
+    //4000 port
+    app.listen( 4000, function () {
+        console.log( 'App listening on port 4000!' );
+    });
+
+
+})
